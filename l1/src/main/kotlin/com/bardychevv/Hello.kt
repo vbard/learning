01e79bb65c50ab83cd0f0b7554e7dabd092e768a
Vasily Bardychev <vbardychev@luxoft.com>
2018-11-04 16:53:41 +0600

    dev::maven default settings

---
+++ l1/src/main/kotlin/com/bardychevv/Hello.kt
@@ -0,0 +1,6 @@
+package com.bardychevv
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ l1/src/test/kotlin/com/bardychevv/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.bardychevv
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
